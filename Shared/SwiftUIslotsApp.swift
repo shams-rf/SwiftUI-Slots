84efe1e94ada695b20f98c7cef70fb620beb2141
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-06-08 08:35:44 +0100

    Initial Commit

---
+++ Shared/SwiftUIslotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIslotsApp.swift
+//  Shared
+//
+//  Created by Shamsuddin Refaei on 08/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIslotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
