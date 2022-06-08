83f5fa7ed7836836b20272fa531685422c2fb2c0
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-06-08 09:17:26 +0100

    Design interface for app and add elements

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var credits = 1000
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        // main container for interface
+        VStack {
+            
+            // display title
+            Text("SwiftUI Slots")
+                .font(.largeTitle)
+                .foregroundColor(.red)
+            
+            Spacer()
+            
+            // display credit score
+            Text("Credits: " + String(credits))
+            
+            Spacer()
+            
+            // display 3 icons
+            HStack {
+                
+                Spacer()
+                
+                Image("apple")
+                
+                Spacer()
+                
+                Image("apple")
+                
+                Spacer()
+                
+                Image("apple")
+                
+                Spacer()
+                
+            }
+            
+            Spacer()
+            
+            Button(" Spin ") {
+                
+                // randomise icons and alter score accordingly
+                
+            }
             .padding()
+            .foregroundColor(.white)
+            .background(Color.red)
+            .font(.title3)
+            .cornerRadius(20)
+            
+            Spacer()
+            
+        }
+        
     }
 }
 
